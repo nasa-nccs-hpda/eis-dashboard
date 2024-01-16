405bae97115767f8fbb482c43166af89a37cf83e
jli <jian.li@nasa.gov>
2024-01-15 21:26:19 -0500

    fixed pep errors

--- eisdashboard/model/rasterDashboard.py
+++ eisdashboard/model/rasterDashboard.py
@@ -1,8 +1,5 @@
 from eisdashboard.model.dashboard import Dashboard
-import eisdashboard.model.utils as utils
-import hvplot.xarray
 import panel as pn
-
 
 
 # -----------------------------------------------------------------------------
@@ -11,85 +8,87 @@ import panel as pn
 # -----------------------------------------------------------------------------
 
 class RasterDashboard(Dashboard):
-    
+
     # -------------------------------------------------------------------------
     # __init__
     # -------------------------------------------------------------------------
-    
+
     def __init__(self, config_file):
         super(RasterDashboard, self).__init__(config_file)
-        
+
         self._basemap = self.getBaseMap()
-        
-        # Populating Variable Select Options        
+
+        # Populating Variable Select Options
         self._interactivityManager._rasterVariableSelectWidget.options = \
-            self._validStarterVariables       
+            self._validStarterVariables
         self._interactivityManager._rasterVariableSelectWidget.value = \
             [self._validStarterVariables[0]]
-        
+
         # Popultating Time Select Options
         self._tstampsOptions = self.initializeTimeStampsOptions()
         self._interactivityManager._rasterTimeSelectWidget.options = \
             self._tstampsOptions
         self._interactivityManager._rasterTimeSelectWidget.value = \
             [self._tstampsOptions[0]]
-            
+
     # ------------------------------------------------------------------------
-    # initializeTimeStampsOptions -- as "initializeVariableOptions" in dashboard.py
+    # initializeTimeStampsOptions
+    # As "initializeVariableOptions" in dashboard.py
     # ------------------------------------------------------------------------
     def initializeTimeStampsOptions(self):
-        
+
         allOptions: list = []
         for key in self._datasetsVariables.keys():
-            tstampsForThisCollection: list = self._datasetsData[key].time.values
+            tstampsForThisCollection = self._datasetsData[key].time.values
             tstampsStr = [f'{e}' for e in tstampsForThisCollection]
-            
+
             allOptions.extend(tstampsStr)
-            
+
         return allOptions
 
     # ------------------------------------------------------------------------
     # Generate raster plot
     # ------------------------------------------------------------------------
-    
-    ## TODO 
-    ## Add option for colormap (cmap)
+
+    # TODO
+    # Add option for colormap (cmap)
     def generateRasterLayer(self, varOpt, timeOpt):
-        
+
         collectionID, variable = self.parseVariableOption(varOpt)
         time = timeOpt
-            
+
         ds = self._datasetsData[collectionID][variable].sel(time=time).load()
-        
-        rasterLayer = ds.hvplot.quadmesh(x='lon', y='lat', 
-                                         geo=True, 
-                                         cmap='jet',
-                                         rasterize=True,
-                                         frame_width=1200,
-                                         title=f'{collectionID}; {variable}; {time}', 
-                                        tiles='EsriImagery',)
-        
-            
+
+        rasterLayer = ds.hvplot.quadmesh(
+            x='lon', y='lat',
+            geo=True,
+            cmap='jet',
+            rasterize=True,
+            frame_width=1200,
+            title=f'{collectionID}; {variable}; {time}',
+            tiles='EsriImagery',
+        )
+
         return rasterLayer
- 
+
     # ------------------------------------------------------------------------
-    # Override getWidgetBox 
+    # Override getWidgetBox
     # ------------------------------------------------------------------------
     def getWidgetBox(self):
         self._logger.info('Getting widget box in RasterDashboard')
-        
+
         widgetBox = pn.WidgetBox(
             self._interactivityManager.rasterVariableSelectWidget,
             self._interactivityManager.rasterTimeSelectWidget,
             height=self.WIDGET_BOX_HEIGHT
-            )
-        
+        )
+
         return widgetBox
-    
+
     # ------------------------------------------------------------------------
     # View
     # ------------------------------------------------------------------------
-          
+
     def view(self):
         """
         Main view for the dashboard. Returns a complete dashboard renderable
@@ -97,11 +96,11 @@ class RasterDashboard(Dashboard):
         Defines and initializes streams, panel compenents
         that need to be defined closest to user invocation.
         """
-        
+
         # Title initializing
         title, subtitle = self.getTitles()
         titleRow = pn.Row(pn.Column(title, subtitle))
-        
+
         # Error and status indicators initialization
         errorRow = pn.Row(
             self._interactivityManager.exceptionCommWidget,
@@ -110,32 +109,34 @@ class RasterDashboard(Dashboard):
         statusRow = pn.Row(
             self._interactivityManager.statusIndicatorWidget,
         )
-        
+
         widgetBox = self.getWidgetBox()
-        
+
         try:
-            
-            rasterPlot = pn.bind(self.generateRasterLayer, \
-                        varOpt=self._interactivityManager.rasterVariableSelectWidget, \
-                        timeOpt=self._interactivityManager.rasterTimeSelectWidget)
+
+            rasterPlot = pn.bind(
+                self.generateRasterLayer,
+                varOpt=self._interactivityManager.rasterVariableSelectWidget,
+                timeOpt=self._interactivityManager.rasterTimeSelectWidget
+            )
+
         except Exception as exceptionCaptured:
             step = 'Initial Generation of the raster plot'
-            
+
             self.excptionHandler(exceptionCaptured, step)
-            # show empty map 
+            # show empty map
             rasterPlot = self.getBaseMap()
-        
+
         self.setWatchers()
-        
+
         rasterRow = pn.Row(rasterPlot, min_height=400,)
-        
+
         lowerRow = pn.Row(widgetBox, rasterRow)
         dashboard = pn.Column(titleRow,
                               errorRow,
                               statusRow,
                               lowerRow,
                               background='WhiteSmoke'
-                             )
+                              )
 
-        
         return dashboard
